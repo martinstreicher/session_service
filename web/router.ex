f359df2b46c2c0fc19e6c451b3cd2166d8f77879
Christopher Stingl <cjstingl@gmail.com>
2016-07-19 15:40:05 -0400

    [CS] Clean up router file & add v1 scope

--- web/router.ex
+++ web/router.ex
@@ -1,26 +1,18 @@
 defmodule SessionService.Router do
   use SessionService.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
-  end
-
-  scope "/", SessionService do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
+    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.LoadResource
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", SessionService do
-  #   pipe_through :api
-  # end
+  scope "/api", SessionService do
+    pipe_through :api
+
+    scope "/v1", V1, as: :v1 do
+      
+    end
+  end
 end
